bd461dea13e23330e1cf89f15aca95e16b1392de
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-28 17:04:04 +0200

    ex-3 rifatto

--- jsnack3/myscript.js
+++ jsnack3/myscript.js
@@ -1,21 +1,11 @@
 //Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero,  se è dispari inseriscilo nell’array.
 
-const arrey=[];
+const arresyList = []
 
-const btn = document.querySelector("button");
-let input= document.querySelector("input")
-
-
-
-btn.addEventListener("click",function(){
-    let x;
-
-    for(let i = 0 ; i == 6; i++){
-        x=input.value
-        console.log(x.value)
-
-
+for(let i=0; i<6; i++){
+    let promptInput = parseInt(prompt("dammi un numero"))
+    if(promptInput % 2 != 0){
+        console.log("numero dispari - aggiunto alla lista")
+        arresyList.push(promptInput)
     }
 }
-);
-   
