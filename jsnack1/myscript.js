707080f67d1de57ff22c524abe1b039ed9d9eb98
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-28 16:43:36 +0200

    ex-1 rifatto

--- jsnack1/myscript.js
+++ jsnack1/myscript.js
@@ -1,12 +1,9 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.//
 let sum = 0;
-
-for (let index = 0; index < 10; index++) {
-   if(parseInt(prompt("Dammi un numero"))!=isNaN){
-    
-        // sum = sum + parseInt(prompt('Insert a number'));
-        sum += parseInt(prompt("Dammi un numero"))
-
-   }
+let promptValue ;
+for(let i=0; i<10;i++){
+   promptValue = parseInt(prompt("dammi un numero"))
+   console.log(promptValue);
+   sum = sum + promptValue;
+   console.log(sum)
 }
-console.log(sum)
